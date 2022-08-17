cd7d0f24272b029549031dd5f0920adbe4954379
kelety <kelety@naver.com>
2022-08-17 18:42:36 +0900

    0817
    0817

---
+++ 0817/hanbun.php
@@ -0,0 +1,11 @@
+<?php
+    // php는 서버사이드 프로그램(동적언어라고도 함) 언어 한때 (asp, php, jsp가 3대 웹 서버 프로그램)
+    // 상대적으로 html을 정적언어라고도 부름
+
+    // .은 php에서 문자열 더하기, echo는 출력문
+    echo $_GET["aaa"]."<br>";
+    echo $_GET["aa"]."<br>";
+    echo "<h1>".$_POSR["bbb"]."</h1><br>";
+    echo "<h1>".$_POSR["yyy"]."</h1><br>";
+?>
+<h1>서버에서 만들기</h1>
